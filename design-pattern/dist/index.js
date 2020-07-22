fa233234097e3862ef1514073c17c584c3dd793e
CregskiN <272473132@qq.com>
2020-07-22 21:52:58 +0800

    代理模式，享元模式

--- design-pattern/dist/index.js
+++ design-pattern/dist/index.js
@@ -6,72 +6,134 @@ function handleKeyDown() {
     console.log('onkeydown触发。div：a我keydown了');
 }
 const elem = document.getElementById('text');
-class User {
-    constructor(mediator) {
-        this.mediator = mediator;
-        this.mediator.registerClient(this);
+// 两种图元
+// 圆形
+class Circle {
+    constructor(name) {
+        this.isHidden = false;
+        this.name = name;
     }
-    getTaxi() {
-        this.taxi = this.mediator.getCar();
-    }
-    pay() {
-        if (this.taxi) {
-            this.mediator.pay(this.taxi);
-            console.log('client pay');
+    draw() {
+        if (!this.isHidden) {
+            this.isHidden = false;
+            console.log(`draw circle: ${this.name}`);
         }
         else {
-            throw new Error('taxi is not define');
+            console.warn(`${this.name} 已经存在了`);
+        }
+    }
+    remove() {
+        if (!this.isHidden) {
+            this.isHidden = true;
+            console.log(`remove circle: ${this.name}`);
+        }
+        else {
+            console.warn(`${this.name} 不存在`);
         }
     }
 }
-class Taxi {
-    constructor(mediator) {
-        this.mediator = mediator;
-        this.isWorking = false;
-        this.mediator.registerCar(this);
+// 矩形
+class Rectangle {
+    constructor(name) {
+        this.isHidden = false;
+        this.name = name;
     }
-    startWork() {
-        this.isWorking = true;
-        this.mediator.updateCarStatus(this);
-        console.log('the car start work');
+    draw() {
+        if (!this.isHidden) {
+            this.isHidden = false;
+            console.log(`draw Rectangle: ${this.name}`);
+        }
+        else {
+            console.warn(`${this.name} 已经存在了`);
+        }
     }
-    finishWork() {
-        this.isWorking = false;
-        this.mediator.updateCarStatus(this);
-        console.log('the car finish work');
+    remove() {
+        if (!this.isHidden) {
+            this.isHidden = true;
+            console.log(`remove Rectangle: ${this.name}`);
+        }
+        else {
+            console.warn(`${this.name} 不存在`);
+        }
     }
 }
-class DiDi {
+/**
+ * 图元工厂
+ * 属性：实例、图元池
+ *
+ *
+ */
+class GraphFactory {
     constructor() {
-        this.clientList = [];
-        this.carList = [];
+        this.graphPool = {}; // 以函数名为 key，数组元素为图元对象
     }
-    registerClient(client) {
-        this.clientList.push(client);
+    getCircle(newName) {
+        return this.getShape(newName, Circle);
     }
-    registerCar(car) {
-        this.carList.push(car);
+    getRectangle(newName) {
+        return this.getShape(newName, Rectangle);
     }
-    getCar() {
-        let car = this.carList.find(car => !car.isWorking);
-        if (car) {
-            car.startWork();
-            return car;
-        }
-        else {
-            throw new Error('there is not a free car, now!');
+    removeAll() {
+        console.log('remove all');
+        for (let item in this.graphPool) {
+            this.graphPool[item].forEach(o => o.isHidden = true);
         }
     }
-    pay(car) {
-        car.finishWork();
+    /**
+     * 遍历图元池，获取某状态的图元数量
+     * @param isHidden
+     */
+    getCount(isHidden) {
+        let count = 0;
+        for (let key in this.graphPool) {
+            // 遍历线程池的索引
+            count = count + this.graphPool[key].filter(s => s.isHidden === isHidden).length;
+        }
+        return count;
     }
-    updateCarStatus(car) {
-        console.log(`车子状态：${car.isWorking ? '工作' : '闲置'}`);
+    // 获取图元
+    getShape(newName, shapeConstructor) {
+        if (this.graphPool[shapeConstructor.name]) {
+            // 图元池 已有该图形，拿出来复用
+            const shape = this.graphPool[shapeConstructor.name].find(s => s.isHidden); // find 返回第一个返回true的元素
+            if (shape) {
+                shape.isHidden = false;
+                shape.name = newName;
+                return shape;
+            }
+        }
+        // 图元池 没有该图形，new 一个push进去
+        const shape = new shapeConstructor(newName);
+        this.graphPool[shapeConstructor.name] = this.graphPool[shapeConstructor.name] || [];
+        this.graphPool[shapeConstructor.name].push(shape);
+        return shape;
     }
 }
-let didi = new DiDi();
-let taxi = new Taxi(didi);
-let user = new User(didi);
-user.getTaxi();
-user.pay();
+GraphFactory.instance = new GraphFactory();
+const num = 5;
+console.log('create elements');
+for (let i = 1; i <= num; i++) {
+    let circle = GraphFactory.instance.getCircle(`circle ${i}`);
+    let rectangle = GraphFactory.instance.getRectangle(`rectangle ${i}`);
+    console.group();
+    console.log(circle);
+    console.log(rectangle);
+    circle.draw();
+    rectangle.draw();
+    console.groupEnd();
+}
+console.log(GraphFactory.instance);
+console.log(`element count: ${GraphFactory.instance.getCount(false)}`);
+GraphFactory.instance.removeAll();
+console.log(`visible element count: ${GraphFactory.instance.getCount(false)}`);
+console.log(`hidden element count: ${GraphFactory.instance.getCount(true)}`);
+console.log('create elements again');
+for (let i = 1; i <= num; i++) {
+    let circle = GraphFactory.instance.getCircle(`new circle ${i}`);
+    let rectangle = GraphFactory.instance.getRectangle(`new rectangle ${i}`);
+    circle.draw();
+    rectangle.draw();
+}
+console.log(`visible element count: ${GraphFactory.instance.getCount(false)}`);
+console.log(`hidden element count: ${GraphFactory.instance.getCount(true)}`);
 //# sourceMappingURL=index.js.map
--- design-pattern/src/index.ts
+++ design-pattern/src/index.ts
@@ -1,112 +1,163 @@
+
+// 图形接口
+interface Shape {
+    isHidden: boolean;
+    name: string;
+    draw: () => void;
+    remove: () => void;
+}
+
+// 两种图元
+// 圆形
+class Circle implements Shape {
+    public isHidden: boolean;
+    public name: string;
+
+    constructor(name: string) {
+        this.isHidden = false;
+        this.name = name;
+    }
+
+    draw() {
+        if (!this.isHidden) {
+            this.isHidden = false;
+            console.log(`draw circle: ${this.name}`);
+        } else {
+            console.warn(`${this.name} 已经存在了`);
+        }
+    }
+
+    remove() {
+        if (!this.isHidden) {
+            this.isHidden = true;
+            console.log(`remove circle: ${this.name}`);
+        } else {
+            console.warn(`${this.name} 不存在`);
+        }
+    }
+}
+
+// 矩形
+class Rectangle implements Shape {
+    public isHidden: boolean;
+    public name: string;
+
+    constructor(name: string) {
+        this.isHidden = false;
+        this.name = name;
+    }
+
+    draw() {
+        if (!this.isHidden) {
+            this.isHidden = false;
+            console.log(`draw Rectangle: ${this.name}`);
+        } else {
+            console.warn(`${this.name} 已经存在了`);
+        }
+    }
+
+    remove() {
+        if (!this.isHidden) {
+            this.isHidden = true;
+            console.log(`remove Rectangle: ${this.name}`);
+        } else {
+            console.warn(`${this.name} 不存在`);
+        }
+    }
+}
+
 /**
- * 对象：1. 用户 、 平台 、车主
- * 用户 行为：叫车
+ * 图元工厂
+ * 属性：实例、图元池
+ * 
+ * 
  */
-interface Client {
-    getTaxi(): void;
-    pay: () => void;
-}
+class GraphFactory {
+    static readonly instance: GraphFactory = new GraphFactory();
+    private graphPool: { [key: string]: Shape[] } = {}; // 以函数名为 key，数组元素为图元对象
 
-interface Car {
-    isWorking: boolean;
-    startWork: () => void;
-    finishWork: () => void;
-}
 
-interface Mediator {
-    registerClient: (client: Client) => void;
-    registerCar: (car: Car) => void;
-    getCar: () => Car;
-    pay: (car: Car) => void;
-    updateCarStatus: (car: Car) => void;
-}
-
-class User implements Client {
-    taxi?: Car;
-    mediator: Mediator;
-
-    constructor(mediator: Mediator) {
-        this.mediator = mediator;
-        this.mediator.registerClient(this);
+    public getCircle(newName: string): Circle {
+        return this.getShape(newName, Circle);
     }
 
-    getTaxi() {
-        this.taxi = this.mediator.getCar();
+    public getRectangle(newName: string): Rectangle {
+        return this.getShape(newName, Rectangle);
     }
 
-    pay() {
-        if (this.taxi) {
-            this.mediator.pay(this.taxi);
-            console.log('client pay');
-        } else {
-            throw new Error('taxi is not define');
-        }
-    }
-}
-
-class Taxi implements Car {
-    isWorking: boolean;
-    mediator: Mediator;
-
-    constructor(mediator: Mediator) {
-        this.mediator = mediator;
-        this.isWorking = false;
-        this.mediator.registerCar(this);
-    }
-
-    startWork() {
-        this.isWorking = true;
-        this.mediator.updateCarStatus(this);
-        console.log('the car start work');
-    }
-
-    finishWork() {
-        this.isWorking = false;
-        this.mediator.updateCarStatus(this);
-        console.log('the car finish work');
-    }
-}
-
-class DiDi implements Mediator {
-    private clientList: Client[];
-    private carList: Car[];
-
-    constructor() {
-        this.clientList = [];
-        this.carList = [];
-    }
-
-    registerClient(client: Client) {
-        this.clientList.push(client);
-    }
-
-    registerCar(car: Car) {
-        this.carList.push(car);
-    }
-
-    getCar() {
-        let car = this.carList.find(car => !car.isWorking);
-        if (car) {
-            car.startWork();
-            return car;
-        } else {
-            throw new Error('there is not a free car, now!');
+    public removeAll() { //删除所有
+        console.log('remove all');
+        for (let item in this.graphPool) {
+            this.graphPool[item].forEach(o => o.isHidden = true);
         }
     }
 
-    pay(car: Car) {
-        car.finishWork();
+    /**
+     * 遍历图元池，获取某状态的图元数量
+     * @param isHidden 
+     */
+    public getCount(isHidden: boolean): number {
+        let count = 0;
+        for (let key in this.graphPool) {
+            // 遍历线程池的索引
+            count = count + this.graphPool[key].filter(s => s.isHidden === isHidden).length;
+        }
+        return count;
     }
 
-    updateCarStatus(car: Car) {
-        console.log(`车子状态：${car.isWorking ? '工作' : '闲置'}`);
+    // 获取图元
+    private getShape(newName: string, shapeConstructor: typeof Circle | typeof Rectangle): Shape {
+        if (this.graphPool[shapeConstructor.name]) {
+            // 图元池 已有该图形，拿出来复用
+            const shape = this.graphPool[shapeConstructor.name].find(s => s.isHidden); // find 返回第一个返回true的元素
+            if (shape) {
+                shape.isHidden = false;
+                shape.name = newName;
+                return shape;
+            }
+        }
+        // 图元池 没有该图形，new 一个push进去
+        const shape = new shapeConstructor(newName);
+        this.graphPool[shapeConstructor.name] = this.graphPool[shapeConstructor.name] || [];
+        this.graphPool[shapeConstructor.name].push(shape);
+        return shape;
     }
-
 }
 
+const num = 5;
+console.log('create elements');
 
-let didi = new DiDi();
-let taxi = new Taxi(didi);
-let user = new User(didi);
-user.getTaxi();
-user.pay();
+
+for (let i = 1; i <= num; i++) {
+    let circle = GraphFactory.instance.getCircle(`circle ${i}`);
+    let rectangle = GraphFactory.instance.getRectangle(`rectangle ${i}`);
+    console.group();
+    console.log(circle);
+    console.log(rectangle);
+    circle.draw();
+    rectangle.draw();
+    console.groupEnd();
+
+}
+console.log(GraphFactory.instance);
+
+
+console.log(`element count: ${GraphFactory.instance.getCount(false)}`);
+
+GraphFactory.instance.removeAll();
+
+console.log(`visible element count: ${GraphFactory.instance.getCount(false)}`);
+console.log(`hidden element count: ${GraphFactory.instance.getCount(true)}`);
+
+console.log('create elements again');
+
+for (let i = 1; i <= num; i++) {
+    let circle = GraphFactory.instance.getCircle(`new circle ${i}`);
+    let rectangle = GraphFactory.instance.getRectangle(`new rectangle ${i}`);
+
+    circle.draw();
+    rectangle.draw();
+}
+
+console.log(`visible element count: ${GraphFactory.instance.getCount(false)}`);
+console.log(`hidden element count: ${GraphFactory.instance.getCount(true)}`);
