c3fe47e5fafda96af8e0bfd750cc0e8836b2cec8
CregskiN <272473132@qq.com>
2020-07-23 22:43:55 +0800

    record

--- design-pattern/dist/index.js
+++ design-pattern/dist/index.js
@@ -6,134 +6,14 @@ function handleKeyDown() {
     console.log('onkeydown触发。div：a我keydown了');
 }
 const elem = document.getElementById('text');
-// 两种图元
-// 圆形
-class Circle {
+class Man {
     constructor(name) {
-        this.isHidden = false;
         this.name = name;
     }
-    draw() {
-        if (!this.isHidden) {
-            this.isHidden = false;
-            console.log(`draw circle: ${this.name}`);
-        }
-        else {
-            console.warn(`${this.name} 已经存在了`);
-        }
-    }
-    remove() {
-        if (!this.isHidden) {
-            this.isHidden = true;
-            console.log(`remove circle: ${this.name}`);
-        }
-        else {
-            console.warn(`${this.name} 不存在`);
-        }
+    earnMoney() {
+        console.log(`earning money`);
     }
 }
-// 矩形
-class Rectangle {
-    constructor(name) {
-        this.isHidden = false;
-        this.name = name;
-    }
-    draw() {
-        if (!this.isHidden) {
-            this.isHidden = false;
-            console.log(`draw Rectangle: ${this.name}`);
-        }
-        else {
-            console.warn(`${this.name} 已经存在了`);
-        }
-    }
-    remove() {
-        if (!this.isHidden) {
-            this.isHidden = true;
-            console.log(`remove Rectangle: ${this.name}`);
-        }
-        else {
-            console.warn(`${this.name} 不存在`);
-        }
-    }
-}
-/**
- * 图元工厂
- * 属性：实例、图元池
- *
- *
- */
-class GraphFactory {
-    constructor() {
-        this.graphPool = {}; // 以函数名为 key，数组元素为图元对象
-    }
-    getCircle(newName) {
-        return this.getShape(newName, Circle);
-    }
-    getRectangle(newName) {
-        return this.getShape(newName, Rectangle);
-    }
-    removeAll() {
-        console.log('remove all');
-        for (let item in this.graphPool) {
-            this.graphPool[item].forEach(o => o.isHidden = true);
-        }
-    }
-    /**
-     * 遍历图元池，获取某状态的图元数量
-     * @param isHidden
-     */
-    getCount(isHidden) {
-        let count = 0;
-        for (let key in this.graphPool) {
-            // 遍历线程池的索引
-            count = count + this.graphPool[key].filter(s => s.isHidden === isHidden).length;
-        }
-        return count;
-    }
-    // 获取图元
-    getShape(newName, shapeConstructor) {
-        if (this.graphPool[shapeConstructor.name]) {
-            // 图元池 已有该图形，拿出来复用
-            const shape = this.graphPool[shapeConstructor.name].find(s => s.isHidden); // find 返回第一个返回true的元素
-            if (shape) {
-                shape.isHidden = false;
-                shape.name = newName;
-                return shape;
-            }
-        }
-        // 图元池 没有该图形，new 一个push进去
-        const shape = new shapeConstructor(newName);
-        this.graphPool[shapeConstructor.name] = this.graphPool[shapeConstructor.name] || [];
-        this.graphPool[shapeConstructor.name].push(shape);
-        return shape;
-    }
-}
-GraphFactory.instance = new GraphFactory();
-const num = 5;
-console.log('create elements');
-for (let i = 1; i <= num; i++) {
-    let circle = GraphFactory.instance.getCircle(`circle ${i}`);
-    let rectangle = GraphFactory.instance.getRectangle(`rectangle ${i}`);
-    console.group();
-    console.log(circle);
-    console.log(rectangle);
-    circle.draw();
-    rectangle.draw();
-    console.groupEnd();
-}
-console.log(GraphFactory.instance);
-console.log(`element count: ${GraphFactory.instance.getCount(false)}`);
-GraphFactory.instance.removeAll();
-console.log(`visible element count: ${GraphFactory.instance.getCount(false)}`);
-console.log(`hidden element count: ${GraphFactory.instance.getCount(true)}`);
-console.log('create elements again');
-for (let i = 1; i <= num; i++) {
-    let circle = GraphFactory.instance.getCircle(`new circle ${i}`);
-    let rectangle = GraphFactory.instance.getRectangle(`new rectangle ${i}`);
-    circle.draw();
-    rectangle.draw();
-}
-console.log(`visible element count: ${GraphFactory.instance.getCount(false)}`);
-console.log(`hidden element count: ${GraphFactory.instance.getCount(true)}`);
+const e = document.getElementById('Bvideo');
+console.log(e);
 //# sourceMappingURL=index.js.map
--- design-pattern/src/index.ts
+++ design-pattern/src/index.ts
@@ -1,163 +1,18 @@
-
-// 图形接口
-interface Shape {
-    isHidden: boolean;
-    name: string;
-    draw: () => void;
-    remove: () => void;
-}
-
-// 两种图元
-// 圆形
-class Circle implements Shape {
-    public isHidden: boolean;
-    public name: string;
-
-    constructor(name: string) {
-        this.isHidden = false;
-        this.name = name;
-    }
-
-    draw() {
-        if (!this.isHidden) {
-            this.isHidden = false;
-            console.log(`draw circle: ${this.name}`);
-        } else {
-            console.warn(`${this.name} 已经存在了`);
-        }
-    }
-
-    remove() {
-        if (!this.isHidden) {
-            this.isHidden = true;
-            console.log(`remove circle: ${this.name}`);
-        } else {
-            console.warn(`${this.name} 不存在`);
-        }
+class Man {
+    job: string;
+    constructor(readonly name: string) { }
+    earnMoney() {
+        console.log(`earning money`)
     }
 }
 
-// 矩形
-class Rectangle implements Shape {
-    public isHidden: boolean;
-    public name: string;
-
-    constructor(name: string) {
-        this.isHidden = false;
-        this.name = name;
-    }
-
-    draw() {
-        if (!this.isHidden) {
-            this.isHidden = false;
-            console.log(`draw Rectangle: ${this.name}`);
-        } else {
-            console.warn(`${this.name} 已经存在了`);
-        }
-    }
-
-    remove() {
-        if (!this.isHidden) {
-            this.isHidden = true;
-            console.log(`remove Rectangle: ${this.name}`);
-        } else {
-            console.warn(`${this.name} 不存在`);
-        }
-    }
+interface HumanRule extends Man {
+    nose: string;
+    mouth: string;
+    ear: string;
+    eye: string;
+    eyebrow: string
 }
 
-/**
- * 图元工厂
- * 属性：实例、图元池
- * 
- * 
- */
-class GraphFactory {
-    static readonly instance: GraphFactory = new GraphFactory();
-    private graphPool: { [key: string]: Shape[] } = {}; // 以函数名为 key，数组元素为图元对象
-
-
-    public getCircle(newName: string): Circle {
-        return this.getShape(newName, Circle);
-    }
-
-    public getRectangle(newName: string): Rectangle {
-        return this.getShape(newName, Rectangle);
-    }
-
-    public removeAll() { //删除所有
-        console.log('remove all');
-        for (let item in this.graphPool) {
-            this.graphPool[item].forEach(o => o.isHidden = true);
-        }
-    }
-
-    /**
-     * 遍历图元池，获取某状态的图元数量
-     * @param isHidden 
-     */
-    public getCount(isHidden: boolean): number {
-        let count = 0;
-        for (let key in this.graphPool) {
-            // 遍历线程池的索引
-            count = count + this.graphPool[key].filter(s => s.isHidden === isHidden).length;
-        }
-        return count;
-    }
-
-    // 获取图元
-    private getShape(newName: string, shapeConstructor: typeof Circle | typeof Rectangle): Shape {
-        if (this.graphPool[shapeConstructor.name]) {
-            // 图元池 已有该图形，拿出来复用
-            const shape = this.graphPool[shapeConstructor.name].find(s => s.isHidden); // find 返回第一个返回true的元素
-            if (shape) {
-                shape.isHidden = false;
-                shape.name = newName;
-                return shape;
-            }
-        }
-        // 图元池 没有该图形，new 一个push进去
-        const shape = new shapeConstructor(newName);
-        this.graphPool[shapeConstructor.name] = this.graphPool[shapeConstructor.name] || [];
-        this.graphPool[shapeConstructor.name].push(shape);
-        return shape;
-    }
-}
-
-const num = 5;
-console.log('create elements');
-
-
-for (let i = 1; i <= num; i++) {
-    let circle = GraphFactory.instance.getCircle(`circle ${i}`);
-    let rectangle = GraphFactory.instance.getRectangle(`rectangle ${i}`);
-    console.group();
-    console.log(circle);
-    console.log(rectangle);
-    circle.draw();
-    rectangle.draw();
-    console.groupEnd();
-
-}
-console.log(GraphFactory.instance);
-
-
-console.log(`element count: ${GraphFactory.instance.getCount(false)}`);
-
-GraphFactory.instance.removeAll();
-
-console.log(`visible element count: ${GraphFactory.instance.getCount(false)}`);
-console.log(`hidden element count: ${GraphFactory.instance.getCount(true)}`);
-
-console.log('create elements again');
-
-for (let i = 1; i <= num; i++) {
-    let circle = GraphFactory.instance.getCircle(`new circle ${i}`);
-    let rectangle = GraphFactory.instance.getRectangle(`new rectangle ${i}`);
-
-    circle.draw();
-    rectangle.draw();
-}
-
-console.log(`visible element count: ${GraphFactory.instance.getCount(false)}`);
-console.log(`hidden element count: ${GraphFactory.instance.getCount(true)}`);
+const e = document.getElementById('Bvideo');
+console.log(e);
