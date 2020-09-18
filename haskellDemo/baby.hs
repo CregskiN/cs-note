7e31bb28180ea87fe1f487e8a66e35da449b9d36
CregskiN <272473132@qq.com>
2020-09-18 08:08:55 +0800

    feat: 🎸 finish pattern matching

--- haskellDemo/baby.hs
+++ haskellDemo/baby.hs
@@ -8,8 +8,8 @@ lostNumbers = [4,8,15,16,23,48]
 -- 遍历 List xs，奇数且小于10替换为"BOOM!"" 奇数切大于10替换为 "BANG!"
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
--- 仿写 length
-length' xs = sum [1 | _ <- xs]
+-- 仿写 length -- List Comprehension
+-- length' xs = sum [1 | _ <- xs]
 
 -- 去除所有小写字母
 removeNoneUppercase :: String -> String
@@ -42,3 +42,37 @@ sayMe x = "Number not between 1 and 5"
 factorial :: (Integral a) => a -> a
 factorial 0 = 1 -- 先捕获0，作为终止条件
 factorial n = n * factorial(n-1)
+
+addVectors :: (Num n) => (n, n) -> (n, n) -> (n, n)
+addVectors (a, b) (c, d) = (a+c, b+d)
+
+addVectors' :: (Num n) => (n, n) -> (n, n) -> (n, n)
+addVectors' a b = (fst a + fst b, snd a + snd b)
+-- 并不好用，无法解决三元组
+
+first :: (a, b, c) -> a
+first (a, _, _) = a
+
+second :: (a, b, c) -> b
+second (_, b, _) = b
+
+head' :: [a] -> a
+head' [] = error "you can't get first item from empty list."
+head' (x:_) = x -- _起到 ...rest 作用
+
+-- 
+tell :: (Show l) => [l] -> String
+tell [] = error "the list is empty."
+tell (x:[]) = "The list has one element " ++ show x
+tell (x:y:[]) = "The list has two elements " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "The list is long. The first two elements are " ++ show x ++ " and " ++ show y
+
+-- 重写 length 递归实现
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+-- 递归实现累加
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
