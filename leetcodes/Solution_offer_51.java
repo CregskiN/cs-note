6a1f354abd6f7cef570506b261452e73974ac1e2
CregskiN <272473132@qq.com>
2020-09-25 23:11:05 +0800

    feat: 🎸 MergeSort: third method to resolve leetcode_offer_51

--- leetcodes/Solution_offer_51.java
+++ leetcodes/Solution_offer_51.java
@@ -4,7 +4,10 @@ class Solution_offer_51 {
   private int res = 0;
 
   public int reversePairs(int[] nums) {
-    return this.method2(nums);
+    // this.method1(nums);
+    // return this.method2(nums);
+    return this.method3(nums);
+
   }
 
   /**
@@ -26,7 +29,7 @@ class Solution_offer_51 {
   }
 
   /**
-   * 归并排序法中，插入统计逆序对
+   * 归并排序法中，维护状态 res 插入统计逆序对
    * 
    * @param nums
    * @return
@@ -34,7 +37,7 @@ class Solution_offer_51 {
   private int method2(int[] nums) {
     this.res = 0;
     int[] temp = new int[nums.length];
-    this.sort(nums, 0, nums.length - 1, temp);
+    this.sort2(nums, 0, nums.length - 1, temp);
     return res;
   }
 
@@ -45,14 +48,14 @@ class Solution_offer_51 {
    * @param r
    * @param temp
    */
-  private void sort(int[] arr, int l, int r, int[] temp) {
+  private void sort2(int[] arr, int l, int r, int[] temp) {
     if (l >= r) {
       return;
     }
     int mid = l + (r - l) / 2;
-    this.sort(arr, l, mid, temp);
-    this.sort(arr, mid + 1, r, temp);
-    this.merge(arr, l, mid, r, temp);
+    this.sort2(arr, l, mid, temp);
+    this.sort2(arr, mid + 1, r, temp);
+    this.merge2(arr, l, mid, r, temp);
   }
 
   /**
@@ -64,7 +67,7 @@ class Solution_offer_51 {
    * @param r    右分支起点下标
    * @param temp 中间数组
    */
-  private void merge(int[] arr, int l, int mid, int r, int[] temp) {
+  private void merge2(int[] arr, int l, int mid, int r, int[] temp) {
     System.arraycopy(arr, l, temp, l, r - l + 1);
     int i = l;
     int j = mid + 1;
@@ -89,6 +92,70 @@ class Solution_offer_51 {
         i++;
       }
     }
+  }
+
+  /**
+   * 第三种方法 使用递归中返回参数，而不是维护Solution类的状态
+   * 
+   * @param nums
+   * @return
+   */
+  private int method3(int[] nums) {
+    int[] temp = new int[nums.length];
+    return this.sort3(nums, 0, nums.length - 1, temp);
+  }
+
+  /**
+   * 
+   * @param arr
+   * @param l    左端下标
+   * @param r    右端下标
+   * @param temp
+   * @return
+   */
+  private int sort3(int[] arr, int l, int r, int[] temp) {
+    if (l >= r) {
+      return 0;
+    }
+    int res = 0;
+    int mid = l + (r - l) / 2;
+    res = this.sort3(arr, l, mid, temp);
+    res += this.sort3(arr, mid + 1, r, temp);
+    if (arr[mid] > arr[mid + 1]) {
+      res += this.merge3(arr, l, mid, r, temp);
+    }
+    return res;
+  }
+
+  private int merge3(int[] arr, int l, int mid, int r, int[] temp) {
+    System.arraycopy(arr, l, temp, l, r - l + 1);
+    int i = l;
+    int j = mid + 1;
+    int res = 0; // 逆序对个数
+
+    for (int k = l; k <= r; k++) {
+      // 为 arr[k] 赋值
+
+      if (i > mid) {
+        // 左分支遍历完成
+        arr[k] = temp[j];
+        j++;
+      } else if (j > r) {
+        // 右分支遍历完成
+        arr[k] = temp[i];
+        i++;
+      } else if (temp[i] > temp[j]) {
+        // 左分支大于右分支
+        arr[k] = temp[j];
+        j++;
+        res = res + mid - i + 1;
+      } else {
+        // 右分支大于等于左分支
+        arr[k] = temp[i];
+        i++;
+      }
+    }
+    return res;
   }
 
   public static void main(String[] args) {
