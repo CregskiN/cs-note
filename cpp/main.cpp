0f25bdfd45533bc83c55c5ea3d558e66b4816760
CregskiN <272473132@qq.com>
2022-02-11 17:23:48 +0800

    feat: 🎸 Primer C++ 10.范型算法

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,33 +1,37 @@
+#include <algorithm>
 #include <array>
 #include <iostream>
+#include <list>
+#include <string>
 #include <vector>
 
 #include "Window.h"
 #include "Window_mgr.h"
 
 int main() {
-    Screen myScreen(5, 5, 'X');
-    myScreen.move(4, 0).set('#').display(std::cout);
-    std::cout << "\n";
-    myScreen.display(std::cout);
-    std::cout << "\n";
+    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::list<int> list1, list2, list3;
+    std::copy(vec.begin(), vec.end(), std::inserter(list1, list1.begin()));
+    std::copy(vec.begin(), vec.end(), std::back_inserter(list2));
+    std::copy(vec.begin(), vec.end(), std::front_inserter(list3));
 
-    std::vector<int> v1 = {1, 2, 3};
-    std::vector<int> v2 = {1, 2, 3, 4};
-    // v1.insert(v1.end(), v2.end() - 2, v2.end());
-    // v1.insert(v1.end(), {3, 4, 5, 6, 7});
-
-    auto i1 = v2.begin(),
-         i2 = v2.end();
-
-    v2.erase(i1, i2 - 1);
-    i1 = v2.begin();
-    i2 = v2.end();
-    
-    while (i1 != i2) {
-        std::cout << *i1 << std::endl;
-        ++i1;
+    for (auto i : list1) {
+        std::cout << i;
+        std::cout << " ";
     }
+    std::cout << std::endl;
+
+    for (auto i : list2) {
+        std::cout << i;
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+
+    for (auto i : list3) {
+        std::cout << i;
+        std::cout << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
