f5920cf4fc376b9954fd7b5e68280e088aeba3f1
CregskiN <272473132@qq.com>
2022-02-11 22:36:50 +0800

    feat: 🎸 Primer C++ 11. 关联容器

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,37 +1,66 @@
-#include <algorithm>
-#include <array>
 #include <iostream>
-#include <list>
+#include <map>
+#include <set>
 #include <string>
-#include <vector>
-
-#include "Window.h"
-#include "Window_mgr.h"
 
 int main() {
-    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::list<int> list1, list2, list3;
-    std::copy(vec.begin(), vec.end(), std::inserter(list1, list1.begin()));
-    std::copy(vec.begin(), vec.end(), std::back_inserter(list2));
-    std::copy(vec.begin(), vec.end(), std::front_inserter(list3));
-
-    for (auto i : list1) {
-        std::cout << i;
-        std::cout << " ";
-    }
-    std::cout << std::endl;
-
-    for (auto i : list2) {
-        std::cout << i;
-        std::cout << " ";
-    }
-    std::cout << std::endl;
-
-    for (auto i : list3) {
-        std::cout << i;
-        std::cout << " ";
-    }
-    std::cout << std::endl;
-
     return 0;
+}
+
+
+/**
+ * @brief 实际转换操作
+ * 
+ * @param s 
+ * @param m 
+ * @return const std::string 
+ */
+const std::string transform(const std::string &s, const std::map<std::string, std::string> &m) {
+    auto map_it = m.find(s);
+    // 如果单词 s 在转换规则 m 中
+    if (map_it != m.cend())
+        return map_it->second;  // 使用替换短语
+    else
+        return s;  // 返回原单词
+}
+
+/**
+ * @brief 外部转化接口
+ * 
+ * @param map_file 
+ * @param input 
+ */
+void word_transform(std::ifstream &map_file, std::ifstream &input) {
+    auto trans_map = buildMap(map_file);
+    std::string text;
+    while (std::getline(input, text)) {  // 读取每个单词
+        std::istringstream stream(text);
+        bool firstword = true;  // 控制是否打印空格
+        while (stream >> word) {
+            if (firstword)
+                firstword = false;
+            else
+                std::cout << " ";
+            std::cout << transform(word, trans_map);
+        }
+        std::cout << std::endl;
+    }
+}
+
+/**
+ * @brief 1. 创建转换映射
+ *
+ * @param map_file
+ * @return std::map<std::string, std::string>
+ */
+std::map<std::string, std::string> buildMap(std::ifstream &map_file) {
+    std::map<std::string, std::string> trans_map;
+    std::string key, value;
+    while (map_file >> key && std::getline(map_file, value)) {  // 读取第一个单词存入key，剩余内容存入 value
+        if (value.size() > 1)
+            trans_map[key] = value.substr(1);  // 跳过隔离空格
+        else
+            throw std::runtime_error("没有提供给" + key + "的规则");
+    }
+    return trans_map;
 }
