e35e61cba909894ad70ce66da9dc83681bb643d4
CregskiN <272473132@qq.com>
2022-03-24 16:22:36 +0800

    feat: 🎸 完成lesson3

--- graphics/tinyrenderer/include/model.cpp
+++ graphics/tinyrenderer/include/model.cpp
@@ -7,7 +7,7 @@
 
 Model::Model(const char *filename) : verts_(), faces_() {
     std::ifstream in;
-    in.open (filename, std::ifstream::in);
+    in.open(filename, std::ifstream::in);
     if (in.fail()) return;
     std::string line;
     while (!in.eof()) {
@@ -17,20 +17,20 @@ Model::Model(const char *filename) : verts_(), faces_() {
         if (!line.compare(0, 2, "v ")) {
             iss >> trash;
             Vec3f v;
-            for (int i=0;i<3;i++) iss >> v.raw[i];
+            for (int i = 0; i < 3; i++) iss >> v[i];
             verts_.push_back(v);
         } else if (!line.compare(0, 2, "f ")) {
             std::vector<int> f;
             int itrash, idx;
             iss >> trash;
             while (iss >> idx >> trash >> itrash >> trash >> itrash) {
-                idx--; // in wavefront obj all indices start at 1, not zero
+                idx--;  // in wavefront obj all indices start at 1, not zero
                 f.push_back(idx);
             }
             faces_.push_back(f);
         }
     }
-    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
+    std::cerr << "# v# " << verts_.size() << " f# " << faces_.size() << std::endl;
 }
 
 Model::~Model() {
@@ -51,4 +51,3 @@ std::vector<int> Model::face(int idx) {
 Vec3f Model::vert(int i) {
     return verts_[i];
 }
-
